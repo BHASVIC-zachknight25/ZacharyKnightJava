cf266dade7c99bab41fee13c8d31da5e4de3fbc2
BHASVIC-zachknight25 <zach.knight25@students.bhasvic.ac.uk>
2026-02-03 09:59:21 +0000

    starting java

---
+++ javaGettinStarted/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    private String getString (String promgt){
+        String ret;
+        Scanner in = new Scanner (System.in);
+        System.out.println(prompt);
+    }
+    public static void main(String[] args) {
+        String ret;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = inp.nextLine();
+        System.out.println("Hello "+ name);
+    }
+}
